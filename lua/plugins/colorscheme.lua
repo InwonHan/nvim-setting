4bca3b599178ebe2bd3b788bc64385f60e8e69a0
Inwon Han <blissgram@gmail.com>
2025-03-19 00:16:52 -0500

    Add code highlight manually in colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,5 +2,10 @@ return {
   "drewtempelmeyer/palenight.vim",
   config = function()
     vim.cmd([[colorscheme palenight]])
+
+    vim.api.nvim_set_hl(0, "@operator", { link = "Keyword" })
+    vim.api.nvim_set_hl(0, "@punctuation.bracket", { fg = "#FFCB6B" })
+    vim.api.nvim_set_hl(0, "@punctuation.delimiter", { link = "Delimiter" })
+    vim.api.nvim_set_hl(0, "@operator.assignment", { link = "Keyword" })
   end,
 }
